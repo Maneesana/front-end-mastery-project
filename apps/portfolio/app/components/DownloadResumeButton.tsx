0257ac492a852304cf6d595e6da068ad112b2250
Maibam Maneesana Singh <maneesanamaibam@outlook.com>
2025-12-01 00:41:09 +0530

    feat:uploaded updated resume

--- apps/portfolio/app/components/DownloadResumeButton.tsx
+++ apps/portfolio/app/components/DownloadResumeButton.tsx
@@ -1,11 +1,11 @@
-'use client'
+'use client';
 
-import PFLButton from "./PFLButton"
+import PFLButton from './PFLButton';
 
-export default function DownloadResumeButton(){
-    const downloadResume = ()=>{
-        const URL ="/April_2025_updated_resume_without_project.pdf"
-        window.open(URL, "_blank")
-      }
-    return <PFLButton onClick={downloadResume}> Download Resume</PFLButton>
+export default function DownloadResumeButton() {
+  const downloadResume = () => {
+    const URL = '/resume_updated_swe_Nov_25_black.pdf';
+    window.open(URL, '_blank');
+  };
+  return <PFLButton onClick={downloadResume}> Download Resume</PFLButton>;
 }
